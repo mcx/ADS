mcx/ADS

commit 483a75afe14a8e4b8ec321391dbb89104be352da

AdsTool: add license onlineinfo
Make the new LicenseAccess::ShowOnlineInfo() available from the command
line.

Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

AdsTool/main.cpp
@@ -451,7 +451,9 @@ int RunLicense(const AmsNetId netid, const uint16_t port, const std::string& gw,
     auto device = bhf::ads::LicenseAccess{ gw, netid, port };
     const auto command = args.Pop<std::string>();
 
-    if (!command.compare("platformid")) {
+    if (!command.compare("onlineinfo")) {
+        return device.ShowOnlineInfo(std::cout);
+    } else if (!command.compare("platformid")) {
         return device.ShowPlatformId(std::cout);
     } else if (!command.compare("systemid")) {
         return device.ShowSystemId(std::cout);

commit 5c08d3ee8c1ebbd7235e23ee835e48282caf6d48

adstool: document license onlineinfo command
We forgot to update the documentation, when we added the new command.

Fixes: 483a75afe14a8e4b8ec321391dbb89104be352da
Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

AdsTool/main.cpp
@@ -131,9 +131,16 @@ COMMANDS:
 		Test if file or directory exists
 		$ adstool 5.24.37.144.1.1 file find --maxdepth=0 'C:/TwinCAT'
 
-	license < platformid | systemid | volumeno >
+	license < onlineinfo | platformid | systemid | volumeno >
 		Read license information from device.
 	examples:
+		Show active licenses on device
+		$ adstool 5.24.37.144.1.1 license onlineinfo
+		OrderNo.;Instances;Status;License
+		TC1000;CPU;Valid;TC3 ADS
+		TC1100;CPU;Valid;TC3 IO
+		TC1200;CPU;Valid;TC3 PLC
+
 		Read platformid from device
 		$ adstool 5.24.37.144.1.1 license platformid
 		50